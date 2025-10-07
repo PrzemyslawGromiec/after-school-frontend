f81a703fcc2f145da974fecf47ab017a890a9600
pgromiec <pgromiec@gmail.com>
2025-10-07 13:37:52 +0100

    REFACTOR: refactor index.js for clarity

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,24 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Lessons from '../views/Lessons.vue'
 import Cart from '../views/Cart.vue'
 
-export default createRouter({
+// export default createRouter({
+//     history: createWebHistory(),
+//     routes: [
+//         {path : '/', component: Lessons},
+//         {path : '/cart', component: Cart}
+//     ],
+//     scrollBehavior: () => ({ top: 0 })
+// });
+
+const routes = [
+    {path : '/', component: Lessons},
+    {path : '/cart', component: Cart}
+];
+
+const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        {path : '/', component: Lessons},
-        {path : '/cart', component: Cart}
-    ],
+    routes,
     scrollBehavior: () => ({ top: 0 })
 });
+
+export default router;
