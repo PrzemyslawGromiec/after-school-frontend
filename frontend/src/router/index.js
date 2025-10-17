f0cac5d5e933c80b4ac405810f27d3a88e149274
pgromiec <pgromiec@gmail.com>
2025-10-17 19:54:31 +0100

    FEATURE: configure GitHub Pages deploy (Vite base + GH Actions + hash router)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter,  createWebHashHistory} from 'vue-router'
 import Lessons from '../views/Lessons.vue'
 import Cart from '../views/Cart.vue'
 import Feedback from '../views/Feedback.vue';
@@ -10,7 +10,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
     scrollBehavior: () => ({ top: 0 })
 });
